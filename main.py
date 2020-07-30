15da247c4d0680f12c5f3f262fe82c900b9d2407
Anthoni Levi Calderon <66741640+anthonilevi@users.noreply.github.com>
2020-07-29 22:03:07 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+nota1 = int(input("ingrese la nota 1: "))
+nota2 = int(input("ingrese la nota 2 :"))
+nota3 = int(input("ingrese la nota 3 :"))
+promedio = ((nota1 + nota2 + nota3) / 3)
+print(promedio)
